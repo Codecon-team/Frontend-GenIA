40a4db14afa65b58ead82cafd11be05853d89833
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2025-05-18 04:18:33 -0300

    feat: criação da tela de dashboard e renomeação da tela de OnlyCurriculum

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,205 +1,215 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { useResumeStore } from '@/stores/resume'
-import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
-const route = useRoute()
-const resumeStore = useResumeStore()
-const id = route.params.id
-
-const pontuacao = ref(0)
-const resumo = ref('')
-const nomeIndividuo = ref('[Nome do individuo]')
-const areaIndividuo = ref('[Área do individuo]')
-const pontosFortes = ref()
-const pontosFracos = ref()
-const loading = ref(false)
-const error = ref(null)
-const user = ref()
-
-onMounted(async () => {
-  user.value = await userStore.getMeUser()
-  await fetchResume()
-})
-
-async function fetchResume() {
-  loading.value = true
-  error.value = null
-  try {
-    const response = await resumeStore.getOneResumeAnalyze(id)
-    console.log(response.resume)
-    if (response) {
-      pontuacao.value = response.resume.analyses[0].score || 0
-      resumo.value = response.resume.analyses[0].funny_comment || 'Nenhum resumo encontrado.'
-      nomeIndividuo.value = user.value?.username || '[Nome não encontrado]'
-      areaIndividuo.value = response.area || '[Área não encontrada]'
-
-      const strengths = response.resume.analyses[0].strengths || ''
-      const weaks = response.resume.analyses[0].weaknesses || ''
-
-      pontosFortes.value = strengths.split(',').map((p) => p.trim())
-      pontosFracos.value = weaks.split(',').map((p) => p.trim())
-    } else {
-      error.value = 'Currículo não encontrado.'
-    }
-  } catch (err) {
-    console.error('Erro ao buscar o currículo:', err)
-    error.value = 'Erro ao buscar o currículo. Tente novamente.'
-  } finally {
-    loading.value = false
-  }
-}
-
+const premium = true;
 </script>
 
 <template>
-  <main v-if="!loading && !error">
-    <span>
-      <div class="glass-container">
-        <p>💥 Pontos Negativos</p>
-        <ul>
-          <li v-for="(ponto, index) in pontosFracos?.slice(0,5)" :key="index">{{ ponto }}</li>
-        </ul>
-        <p>🤏 Ponto Positivos</p>
-        <ul>
-          <li v-for="(ponto, index) in pontosFortes?.slice(0,5)" :key="index">{{ ponto }}</li>
-        </ul>
-
-      </div>
-      <div class="glass-container">
-        <div class="progress-text">
-          <span>
-            <p v-if="pontuacao < 33">Bucha 💀</p>
-            <p v-else-if="pontuacao < 66">Dá pro gasto 🤏</p>
-            <p v-else-if="pontuacao < 90">Razoável 🤙</p>
-            <p v-else>Diamante do gueto 💎</p>
-          </span>
-          <span> {{ pontuacao }}%</span>
+  <main>
+    <div class="user">
+      <div class="info-user">
+        <div class="glass-container">
+          <h1>[Nome da pessoa]</h1>
+          <p>Apareceu para incomodar em [data]</p>
+          <p>Mudou de identidade em: [data]</p>
         </div>
-        <progress :value="pontuacao" max="100" />
+        <div class="glass-container">
+          <h1>classe: [classe]</h1>
+          <p>Assinatura vai até [data]</p>
+          <p>Ultimo pagamento em [data]</p>
+        </div>
       </div>
-      <label class="green-btn">
-        Upload
-        <input type="file" id="file" name="file" />
-      </label>
-    </span>
-    <div class="info glass-container">
-      <span class="title-container">
-        <h1>{{ nomeIndividuo }}</h1>
-        <h2>{{ areaIndividuo }}</h2>
-      </span>
-      <h3>Resumo:</h3>
-      <p>{{ resumo }}</p>
+      <div class="bottom-container">
+        <div class="user-form glass-container">
+          <span class="inputs-container">
+            <label>
+              <p>Primeiro Nome</p>
+              <input type="text" />
+            </label>
+            <label>
+              <p>Segundo nome</p>
+              <input type="text" />
+            </label>
+            <label>
+              <p>Email</p>
+              <input type="text" />
+            </label>
+            <label>
+              <p>CPF</p>
+              <input type="text" />
+            </label>
+          </span>
+          <button class="green-btn">Salvar</button>
+        </div>
+        <div class="upload-cv">
+          <p>Faça o Upload do Arquivo aqui mano, relaxa que tá seguro!</p>
+          <label class="green-btn"
+            >Upload CV<input type="file" id="file" name="file"
+          /></label>
+        </div>
+      </div>
+    </div>
+    <div class="curriculums-container">
+      <h2>Histórico</h2>
+      <div class="curriculum-card">
+        <img
+          src="https://www.dicasdecurriculo.com.br/wp-content/uploads/2024/03/modelo-de-curriculo-word-simples.jpg"
+          alt=""
+        />
+        <span>
+          <p>[data curriculo]</p>
+          <p>[nota curriculo]</p>
+        </span>
+      </div>
     </div>
   </main>
-  <div v-else-if="loading">Carregando informações do currículo...</div>
-  <div v-else-if="error">Erro ao carregar o currículo: {{ error }}</div>
 </template>
 
 <style scoped>
-.info {
-  width: 64vw;
-
-  height: 63vh;
-
-  padding: 5vh 3vw;
-}
-
-p {
-  font-size: 20px;
-
-  text-align: justify;
-}
-
-.title-container {
-  border-bottom: #000000 1px solid;
-
-  padding-bottom: 3vh;
-
-  margin-bottom: 3vh;
-}
-
-progress {
-  margin-top: 0.5vw;
-
-  border-radius: 7px;
-
-  width: 100%;
-}
-
-progress::-webkit-progress-bar {
-  background-color: #ffffff;
-
-  border-radius: 7px;
-}
-
-progress::-webkit-progress-value {
-  background-color: #1a5d46;
-
-  border-radius: 7px 0 0 7px;
-}
-
-.progress-text {
+.upload-cv{
   display: flex;
-
-  justify-content: space-between;
-
-  font-size: 20px;
-
-  font-weight: 700;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1vh 1vw;
+  width: 20%;
+  height: 20vh;
+  border-radius: 10px;
+  background-color: #ffffff5e;
+  border: dashed #1a5d46 1px;
+  gap: 2vh;
+  text-align: center  ;
 }
 
-label {
-  text-align: center;
+.upload-cv p {
+  font-size: 2vh;
 }
 
-input {
+.upload-cv > label{
+  font-size: 2vh;
+}
+
+.upload-cv > label  input {
   display: none;
 }
 
-main > div {
-  background-color: #d9d9d959;
+.bottom-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
-  padding: 3vh 2vw;
+.bottom-container > label {
+  width: 20%;
+  text-align: center;
+  height: fit-content;
+}
+
+.curriculum-card {
+  padding: 2vh 0;
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 5vw;
+}
+.curriculum-card img {
+  width: 2vw;
+  box-shadow: 6px 6px 10px rgba(128, 128, 128, 0.346);
+}
+
+h2 {
+  margin-bottom: 3vh;
+}
+
+.curriculums-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  width: 23vw;
+  max-height: 70vh;
+  padding: 3vh 0;
+  overflow-y: scroll;
+  box-shadow: 6px 6px 15px rgba(128, 128, 128, 0.346);
+}
+
+.inputs-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2vw;
+}
+
+.user-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4vh;
+  border-radius: 10px;
+  max-width: 60%;
+  box-shadow: 6px 6px 15px rgba(128, 128, 128, 0.346);
+  padding: 4vh 2vw;
+}
+
+.user-form button {
+  margin-top: 3vh;
+}
+
+label p {
+  font-size: 2vh;
+}
+
+.user-form input {
+  padding: 0.5vh 0.4vw;
+  border-radius: 5px;
+  outline: none;
+  border: none;
+}
+
+.user-form input:enabled {
+  border: #1a5d46 1px solid;
+}
+
+.user {
+  width: 55vw;
+  display: flex;
+  flex-direction: column;
+  gap: 5vh;
+}
+
+.info-user > div {
+  position: inherit;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 4vh 0;
+  border-radius: 10px;
+  box-shadow: 6px 6px 15px rgba(128, 128, 128, 0.346);
+}
+
+.info-user > div > p {
+  font-size: 2vh;
+  margin-bottom: 2vh;
+}
+
+h1 {
+  z-index: 3;
+  font-size: 3vh;
+  margin-bottom: 3vh;
 }
 
 main {
+  padding: 8vh 8vw;
   display: flex;
-
-  padding-top: 8vh;
-
-  justify-content: space-around;
+  justify-content: space-between;
 }
 
-main span {
+.info-user {
   display: flex;
-
-  flex-direction: column;
-
-  gap: 2vh;
-}
-
-main span .glass-container {
-  display: flex;
-
-  flex-direction: column;
-
-  width: 20vw;
-
-  background-color: #d9d9d959;
-}
-
-main span .glass-container p {
-  font-size: 20px;
-
-  font-weight: 700;
-}
-
-main span .glass-container:first-of-type {
-  height: 100%;
-
-  gap: 1vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: relative;
+  height: 25vh;
+  max-width: 100%;
+  gap: 5vw;
 }
 </style>
