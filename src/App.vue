6e3dc54711eb670597830d325753f06c9d6123c5
Eduardo Montandon <emontandon11@gmail.com>
2025-05-17 23:12:19 -0300

    refactor: replace buttons with RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup>
-
-
-
-
-
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <button @click="handleLogin()">testa covarde</button>
-  <button @click="handleRegister()">register</button>
-  <button @click="handleUpdate()">teste</button>
-  <button @click="handleGetMeUser()">getUSERinfo</button>
+  <RouterView />
 </template>
 
 <style scoped>
