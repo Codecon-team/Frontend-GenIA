0e410916c76ca824a14940ca4980b8489ff9e9be
Eduardo Montandon <emontandon11@gmail.com>
2025-05-17 23:09:22 -0300

    feat: integrate user authentication with API and state management
    - Added @vueuse/core as a dependency for reactive storage.
    - Implemented UserService for handling user-related API calls (login, register, update, get user info).
    - Created a user store using Pinia for managing user state and authentication.
    - Set up Axios interceptors for attaching authorization tokens to requests.
    - Updated App.vue to include buttons for testing user actions.
    - Enhanced main.js to initialize API interceptors.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { RouterView } from 'vue-router'
+
+
+
+
+
 </script>
 
 <template>
-  <RouterView />
+  <button @click="handleLogin()">testa covarde</button>
+  <button @click="handleRegister()">register</button>
+  <button @click="handleUpdate()">teste</button>
+  <button @click="handleGetMeUser()">getUSERinfo</button>
 </template>
 
 <style scoped>
