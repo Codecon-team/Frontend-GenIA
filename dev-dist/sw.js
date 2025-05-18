df7a9c66025de9c91bc1bdafc1dec810e578716f
Eduardo Montandon <emontandon11@gmail.com>
2025-05-18 04:03:11 -0300

    FEAT: update dashboard route to include user ID and enhance DashboardView with resume fetching logic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a7pv010q4dg"
+    "revision": "0.oip9nc40h6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,7 +10,7 @@ const router = useRouter()
 const path = mdiArrowLeft
 
 const user = ref({
-    email: "limateste@gabigol.com",
+    email: "lima@gabigol.com",
     password: "pedroguilherme"
 })
 
