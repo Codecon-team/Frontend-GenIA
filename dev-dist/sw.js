e6f31ee8c398bee438df1bdb3a358687ce2ac674
danielhinsching <danielsantoshinsching@gmail.com>
2025-05-18 01:51:19 -0300

    CHORE: atualização main css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -34,6 +34,7 @@ if (!self.define) {
             resolve();
           }
         })
+      
       .then(() => {
         let promise = registry[uri];
         if (!promise) {
@@ -80,7 +81,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5k759jinnso"
+    "revision": "0.itiu2bvr2f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
